794dc88b3e0f6536cbdb11fcdac7e12e724ee102
Sara Aljawabrah <saljawab@c1r2s6.42amman.com>
2026-04-05 20:24:28 +0300

    5/4/2026

---
+++ c02/ex01/ft_strncpy.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saljawab <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/05 16:13:48 by saljawab          #+#    #+#             */
+/*   Updated: 2026/04/05 20:13:32 by saljawab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+/*
+int main()
+{
+	char	*a = "abcefg";
+	char	b[7];
+        ft_strncpy(b, a, 7);
+	
+	printf("%s\n",b);
+
+	return 0;
+}*/
