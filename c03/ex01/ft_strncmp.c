dbbc15100ee5ba7062f22c841e8b2a4c4a2c0662
Sara Aljawabrah <saljawab@c1r4s5.42amman.com>
2026-04-10 18:30:36 +0300

    idk smth

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -20,7 +20,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 		return (0);
 	while (i < n)
 	{
-		if (!(s1[i] || s1[i] == s2[i]))
+		if (!(s1[i] || s2[i] || s1[i] == s2[i]))
 			return (s1[i] - s2[i]);
 		i++;
 	}
